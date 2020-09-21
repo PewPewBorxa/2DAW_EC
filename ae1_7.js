a24b51326af4895af781410a1ceb436695e2a240
botolo <datborx@gmail.com>
2020-09-21 09:46:56 +0200

    AE1-7

---
+++ ae1_7.js
@@ -0,0 +1,7 @@
+//7 Escribe una función que acepte como parámetro un número y devuelva el valor absoluto del mismo
+
+function valorAbsoluto(num){
+    if(num<0){
+        return (-1*num);
+    }else{return num;}
+}
