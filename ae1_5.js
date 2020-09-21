9278555c9bf98c9c80c9dc7d9cca04bfcccd1480
botolo <datborx@gmail.com>
2020-09-21 09:45:20 +0200

    AE1_5

---
+++ ae1_5.js
@@ -0,0 +1,7 @@
+//5 Escribe el codigo necesario para calcular el factorial del numero 15, en menos de 5 instrucciones
+
+var num = 15;
+for(i=(num-1);i>=1;i--){
+    num= num*i;
+}
+console.log(num);
