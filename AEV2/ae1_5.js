ff1005380ef6b8ffb09ea0b76faa3a06d7f2f37e
botolo <datborx@gmail.com>
2020-09-30 19:06:37 +0200

    AE2-5

--- AEV2/ae1_5.js
+++ AEV2/ae1_5.js
@@ -1,3 +1,4 @@
 function multipleFactorial(values=[]){
-    return values.map(Math.multipleFactorial)
+    const factorial = (number)=> (number == 1) ? 1 : (number * factorial(number - 1));
+    return values.map(factorial);
 }
